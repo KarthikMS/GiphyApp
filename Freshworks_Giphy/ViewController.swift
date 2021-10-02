5b7e4da002d054bd1c9b399838ec810faa7f2dcb
KarthikMx <karthik.ms@mxplayer.in>
2021-10-02 22:16:55 +0530

    Initial Commit

---
+++ Freshworks_Giphy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Freshworks_Giphy
+//
+//  Created by Karthik Maharajan Skandarajah on 02/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
